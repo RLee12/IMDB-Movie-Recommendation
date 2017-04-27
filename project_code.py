e2cc07eb7b02411a6eb197014df659e6405d0d9e
RLee12 <m519160201@gmail.com>
2017-04-27 13:06:42 -0700

    Update project_code.py

--- project_code.py
+++ project_code.py
@@ -37,3 +37,38 @@ reviews_subset = reviews_df.iloc[random_idx].reset_index()
 # reviews_subset.to_csv(getcwd() + "/amazon_reviews.csv")
 # reviews_df.to_csv(getcwd() + "/amazon_reviews_full.csv")
 # meta_df.to_csv(getcwd() + "/amazon_meta.csv")
+
+# From now on we work on this subset of the original dataset.
+reviews_df = pd.read_csv("amazon_reviews.csv")
+
+# Number of different reviewer names in the data.
+len(set(reviews_df.reviewerName))
+
+# Plot the user rating distribution.
+
+N_star_categories = 5
+colors = np.array(['#E50029', '#E94E04', '#EEC708', '#A5F30D', '#62F610'])
+star_labels = np.array([star_label+1 for star_label in range(N_star_categories)])
+star_category_dist_fig = plt.figure(figsize = (10, 8), dpi = 100)
+bar_plot_indices = np.arange(N_star_categories)
+star_category_absolute_frequencies = reviews_df.overall.value_counts(ascending = True)
+star_category_relative_frequencies = (np.array(star_category_absolute_frequencies)/
+                                      float(sum(star_category_absolute_frequencies)))
+rects = (plt.bar(bar_plot_indices, star_category_relative_frequencies, width = 1, 
+                 color = sns.color_palette("YlOrRd", 5), alpha = .7))
+
+for (idx, rect) in enumerate(rects):
+        plt.gca().text(rect.get_x() + rect.get_width()/2., 1.05*rect.get_height(), 
+                       '%.3f'%(star_category_relative_frequencies[idx]), ha = 'center', va = 'bottom')
+
+plt.xticks(bar_plot_indices + .5, star_labels)
+plt.xlabel('Rating Category')
+plt.ylabel('Relative Frequency')
+plt.ylim([0, 1])
+plt.title('User Rating Distribution for {} Amazon Movie and TV Reviews'.format(len(reviews_df)))
+
+plt.show()
+
+
+
+
